79da6cb83df0d039f032cbdcb66f6756dd00082a
djjydbb <djjydbb@naver.com>
2018-02-04 23:54:01 +0900

    1st

---
+++ script.js
@@ -0,0 +1,10 @@
+$(".container").eq(0).delay().fadeIn(600, "easeInQuint");
+$(".container").eq(1).delay(400).fadeIn(600, "easeInQuint");
+$(".container").eq(2).delay(800).fadeIn(600, "easeInQuint");
+$(".container").eq(3).delay(1200).fadeIn(600, "easeInQuint");
+$(".container").eq(4).delay(1600).fadeIn(600, "easeInQuint");
+$(".container").eq(5).delay(2000).fadeIn(600, "easeInQuint");
+$(".container").eq(6).delay(2400).fadeIn(600, "easeInQuint");
+$(".container").eq(7).delay(2800).fadeIn(600, "easeInQuint");
+$(".more").delay(3200).fadeIn(1000, "easeInQuint");
+$(".footer").delay(3600).fadeIn(1000, "easeInQuint");
